453b265c039062acd8f7357ed1333a695b90b390
HanxingYf <1919138974@qq.com>
2020-11-28 20:46:03 +0800

    git command

--- leetcode/twoSum/2.js
+++ leetcode/twoSum/2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
   * @author yf
   * @data 20-11-20
@@ -27,34 +26,4 @@ const twoSum = (nums, target) => {
     return res;
 }
 
-=======
-/**
-  * @author yf
-  * @data 20-11-20
-  * @param {number[]} nums
-  * @param {number} target
-  * @return {number[]}
-  */
-
-//箭头函数
-const twoSum = (nums, target) => {
-    // console.log('两数相加');
-    let map = {};   //JSON Object HashMap 空间复杂度的开销 O(n)
-    let res = [];
-    //编写功能 一个函数 一个功能
-    //callback
-    nums.forEach((e, i) => map[e] = i)  //O(n)
-    console.log(map);
-    //?
-    for(let i = 0; i < nums.length; i++){
-        let j = map[target - nums[i]];
-        if(j && j != i){
-            res = [i, j];
-            break;
-        }
-    }
-    return res;
-}
-
->>>>>>> c72819b27d30fb6c453b1bcc94794c00951d78c3
 twoSum([2, 7, 11, 15], 9);
