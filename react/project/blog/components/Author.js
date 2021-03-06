a55d445a9ca142f3a5bc8bc3f1ca4fb3336291e0
yf <1919138974@qq.com>
2021-03-06 21:00:21 +0800

    blog

---
+++ react/project/blog/components/Author.js
@@ -0,0 +1,21 @@
+import { Avatar, Divider } from 'antd'
+
+const Author = () => {
+
+  return (
+    <div className="author-div comm-box">
+      <div> <Avatar size={100} src="https://sf3-ttcdn-tos.pstatp.com/img/user-avatar/c14a19d241220a025e642bc41a2d6aae~300x300.image" /></div>
+      <div className="author-introduction">
+        江西财经大学
+        <br></br>大三学生
+        <Divider>社交账号</Divider>
+        <Avatar size={28}  src="https://s3.ax1x.com/2021/03/06/6nxq8x.jpg"  className="account" />
+        <Avatar size={28} src="https://s3.ax1x.com/2021/03/06/6nxbP1.jpg" className="account" />
+        <Avatar size={28}  src="https://s3.ax1x.com/2021/03/06/6nx75R.jpg" className="account" />
+
+      </div>
+    </div>
+  )
+}
+
+export default Author
