42d1455da471413b5cfb4c6a7520f3f5951b1d82
yf <1919138974@qq.com>
2021-03-07 15:21:40 +0800

    quickSort

--- leetcode/100题/241.js
+++ leetcode/100题/241.js
@@ -1,9 +1,20 @@
-/*
- * @param {string} input
- * @return {number[]}
- * @author 前端羊
- */
+const quickSort = (nums) => {
+  let left = [];
+  let right = [];
+  let pivot = Math.floor(nums.length / 2);
+  // console.log(pivot);
+  let base = nums.splice(pivot, 1)[0];
+  // let base = nums[pivot];
+  // console.log(nums,pivot);
+  if (nums.length < 2) return nums;
+  else {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] < base) left.push(nums[i]);
+      else right.push(nums[i]);
+    }
+  }
+  return quickSort(left).concat([base], quickSort(right));
+}
 
-var diffWaysToCompute = function (input) {
-
-};
+let nums = [1, 34, 5, 76, 8, 6, 9, 7, 6, 3];
+console.log(quickSort(nums));
