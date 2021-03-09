854e675c7da274d5bb80f1bae2abb0a86dd8381d
yf <1919138974@qq.com>
2021-03-10 00:50:07 +0800

    blog

--- react/project/servive/app/controller/default/home.js
+++ react/project/servive/app/controller/default/home.js
@@ -12,6 +12,22 @@ class HomeController extends Controller{
         this.ctx.body=result
     }
 
+    async getArticleList(){
+
+        let sql = 'SELECT article.id as id,'+
+                  'article.title as title,'+
+                  'article.introduce as introduce,'+
+                  'article.addTime as addTime,'+
+                  'article.view_count as view_count ,'+
+                  '.type.typeName as typeName '+
+                  'FROM article LEFT JOIN type ON article.type_id = type.Id'
+     
+         const results = await this.app.mysql.query(sql)
+     
+         this.ctx.body={
+             data:results
+         }
+     }
 
 }
 
