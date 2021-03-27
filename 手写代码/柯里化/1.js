9f8c96edb234f90c430c89be1aea3aebcc92f045
yf <1919138974@qq.com>
2021-03-27 23:17:28 +0800

    新分支

--- 手写代码/柯里化/1.js
+++ 手写代码/柯里化/1.js
@@ -1,49 +1,14 @@
-// 个人理解：使用 bind 进行简单的柯里化
-
-// 版本一 this 绑定不明确，可能造成难以追踪的 bug
-function foo(a, b) {
-  console.log("a : " + a + ", b : " + b);
+// 普通的add函数
+function add(x, y) {
+  return x + y
 }
-var bar = foo.bind(null, 2);  // 因为我们不需要关心 this 的绑定对象，所以使用 null 作为一个占位值
-bar(3);
 
-// 版本二 使用非军事区
-function foo(a, b) {
-  console.log("a : " + a + ", b : " + b);
-}
-// DMZ(非军事区)
-var MyDMZ = Object.create(null);
-
-foo.apply(MyDMZ, [2, 3]); // 将数组展开成参数
-
-var bar = foo.bind(MyDMZ, 2);  
-bar(3);
-
-
-
-// 只能看懂第二版的简易代码……
-function sub_curry(fn) {
-  return function () {
-    return fn()
+// Currying后
+function curryingAdd(x) {
+  return function (y) {
+      return x + y
   }
 }
 
-function curry(fn, length) {
-  length = length || 4;
-  return function () {
-    if (length > 1) {
-      return curry(sub_curry(fn), --length)
-    }
-    else {
-      return fn()
-    }
-  }
-}
-
-var fn0 = function () {
-  console.log(1)
-}
-
-var fn1 = curry(fn0)
-
-fn1()()()() // 1
+add(1, 2)           // 3
+curryingAdd(1)(2)   // 3
