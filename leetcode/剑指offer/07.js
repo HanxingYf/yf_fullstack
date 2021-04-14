fae0e0097e3fefd6f39d05538ed5b547f3eb13da
yf <1919138974@qq.com>
2021-04-14 23:20:03 +0800

    lc

--- leetcode/剑指offer/07.js
+++ leetcode/剑指offer/07.js
@@ -10,13 +10,21 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-var buildTree = function (preorder, inorder) {
-    let root = preorder[0];
-    console.log(inorder.indexOf(root));
+ var buildTree = function (preorder, inorder) {
+    if (preorder.length === 0) {
+        return null;
+    }
+    // 根节点
+    let cur = new TreeNode(preorder[0]);
+    // 中序遍历中根节点的index
+    const index = inorder.indexOf(preorder[0]);
+    cur.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index));
+    cur.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1))
+    return cur;
 };
 
 preorder = [3, 9, 20, 15, 7]
-inorder = [9, 3, 15, 20, 7]
+inorder = [9, 3, 15, 20, 7] // index = 1
 console.log(buildTree(preorder, inorder));
 
-// 5 9 4 3 15 20 7
+// 5 9 3 15 20 7
