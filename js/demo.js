f6ba31cd33d0fa082b9a78f74dc8e52b99efbc1c
HanxingYf <1919138974@qq.com>
2021-11-16 19:24:35 +0800

    demo

--- js/demo.js
+++ js/demo.js
@@ -1,17 +1,9 @@
-function foo(a,b){
-  console.log(b);
-  return {
-    foo:function(c){
-      return foo(c,a);
-    }
-  }
+function  eval (str) {
+  return new Function(`return ${str}`)
 }
- 
-// var func1=foo(0);
-// func1.foo(1);
-// func1.foo(2);
-// func1.foo(3);
-// var func2=foo(0).foo(1).foo(2).foo(3);
-var func3=foo(0).foo(1);
-func3.foo(2);
-func3.foo(3);
+
+// console.log(eval('poi')());
+
+let str = 'return ' + '`Hello ${name}!`';
+let func = new Function('name', str);
+func('Jack') // "Hello Jack!"
