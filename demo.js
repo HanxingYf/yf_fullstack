bf93ab9d7bdabb4cd263e4724bbf404e1e87a729
HanxingYf <1919138974@qq.com>
2021-12-14 20:35:56 +0800

    1

--- demo.js
+++ demo.js
@@ -1,15 +1,32 @@
-function sumT(a, b) {
-    return  new Promise((resolve, reject) => {
-        asyncAdd(a, b, (err, res) => {
-            if(!err) {
-                resolve(res)
-            }
-            reject(err)
-        })
-    })
-}
+function pointToSegmentDist(p, a, b) {
+    var AB = [b[0] - a[0], b[1] - a[1]]
+    var AP = [p[0] - a[0], p[1] - a[1]]
+  
+    var AB_AP = AB[0] * AP[0] + AB[1] * AP[1]
+    var distAB2 = AB[0] * AB[0] + AB[1] * AB[1]
+  
+    var D = [a[0], a[1]]
+    if (distAB2 != 0) {
+      var t = AB_AP / distAB2
+  
+      if (t > 1) {
+        D = [b[0], b[1]]
+      } else if (t > 0) {
+        D = [a[0] + AB[0] * t, a[1] + AB[1] * t]
+      } else {
+        D = [a[0], a[1]]
+      }
+    }
+  
+    var AD = [p[0] - a[0], p[1] - a[1]]
+  
+    return Math.sqrt(AD[0] * AD[0] + AD[1] * AD[1])
+  }
 
-// 测试
-const test = await sumT(1, 2)
-console.log(test)
-// 3
+
+  let p = [3,3]
+  let a = [0,0]
+  let b = [2,0]
+
+  console.log(pointToSegmentDist(p, a, b));
+  console.log(Math.sqrt(10));
--- js/demo.js
+++ js/demo.js
@@ -1,9 +1,18 @@
-function  eval (str) {
-  return new Function(`return ${str}`)
-}
-
-// console.log(eval('poi')());
-
-let str = 'return ' + '`Hello ${name}!`';
-let func = new Function('name', str);
-func('Jack') // "Hello Jack!"
+const res = {
+  errno: 0,
+  search_id: "0a0f213261a499e524c568bd08a48102",
+  country_id: 52,
+  country_code: "MEX",
+  canonical_country_code: "MX",
+  city_id: 52090100,
+  city: "墨西哥城",
+  result: [
+    {
+      poiid: "ChIJif2BL83-0YURv2yOnoW8Wo4",
+      lng: -99.132301822723,
+      lat: 19.432649381333,
+      coordinate_type: "wgs84",
+      srctag: "nearroad",
+    },
+  ],
+};
